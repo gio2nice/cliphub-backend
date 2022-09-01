669099427452dd0d5fb2933fb4c806c40d6e19c3
Noah Carralero <nole2kgames@gmail.com>
2022-09-01 19:16:35 -0400

    Fixed routes and missing index file

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/api', apiRoutes);
+router.use(homeRoutes)
+
+//GMS not sure whether we need this router end or not in index. leaving it here for now
+// router.use((req, req) => {
+//     res.status(404).end();
+// })
+
+module.exports = router;
