b9f3566b800bd4b68e91b6f97c1fffeb683f292c
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-02 10:33:57 -0700

    index.js

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Customer = require('./customer-model');
+const Appointment = require('./appointment-model');
+const Service = require('./service-model');
+const Barber = require('./barber-model');
+
+// Associations
+
+
+
+module.exports = {
+    Customer,
+    Appointment,
+    Service,
+    Barber
+};
