2df1a05f1187cbcf0c00ca393c8537f6f2f6a70c
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-06 10:53:40 -0700

    test

--- server.js
+++ server.js
@@ -1,33 +1,76 @@
-const express = require("express");
-const session = require("express-session");
-const cors = require("cors");
-const { sequelize } = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const express = require("express");
+// const session = require("express-session");
+// const cors = require("cors");
+// const { sequelize } = require("./config/connection");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// require('dotenv').config();
+
+// const sess = {
+//     secret: process.env.SESS_SECRET,
+//     cookie: {sameSite: true},
+//     resave: false,
+//     saveUnitialized: false,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+// // app.set('trust proxy', 1)
+// app.use(session(sess));
+
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+// app.use(cors({credentials: true}));
+
+// app.use('/api', require("./routes"));
+
+// app.listen(port, () => {
+//     console.log(`Listening on port ${port}`);
+//     sequelize.sync({ force: false });
+// }) 
+const express = require('express');
+const routes = require('./controllers');
+const path = require('path');
+const sequelize = require('./config/connection');
+
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-require('dotenv').config();
+const session = require('express-session');
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.SESS_SECRET,
-    cookie: {sameSite: true},
-    resave: false,
-    saveUnitialized: false,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
-// app.set('trust proxy', 1)
+
 app.use(session(sess));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cors({credentials: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.use('/api', require("./routes"));
+// turn on routes
+app.use(routes);
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-    sequelize.sync({ force: false });
-})
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on Port ${PORT}!! `));
+});
