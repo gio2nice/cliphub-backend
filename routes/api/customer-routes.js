46f82c4acfe227f94b10b364311cf90ed620c7a4
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-20 15:12:19 -0700

    changes

--- routes/api/customer-routes.js
+++ routes/api/customer-routes.js
@@ -5,21 +5,21 @@ const app = express();
 const bcrypt = require('bcrypt');
 
 // find all customers
-router.get('/', (req, res) =>{
-    
-    
-     Customer.findAll()
-     .then(data => {
-         res.status(200).json(data)
-     }).catch(err => {
-         console.log(err)
-         res.status(500).json({err, msg: 'error occured'})
-     })
-        
+router.get('/', (req, res) => {
+
+
+    Customer.findAll()
+        .then(data => {
+            res.status(200).json(data)
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json({ err, msg: 'error occured' })
+        })
+
 })
 
 // create a customer
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     // if(!req.session.loggedIn){
     //     res.status(403).json({msg:"must login first!"})
     // }
@@ -29,24 +29,24 @@ router.post('/', async (req,res) => {
         });
 
         res.status(200).json(newCustomer);
-    }   catch (err) {
+    } catch (err) {
         res.status(400).json(err);
     }
 });
 
 //find one customer
-router.get("/:id",(req,res)=>{
+router.get("/:id", (req, res) => {
     // if(!req.session.loggedIn){
     //     res.status(403).json({msg:"must login first!"})
     // }
     Customer.findOne({
-        where:{
-            id:req.params.id
+        where: {
+            id: req.params.id
         }
-    }).then(data=>{
+    }).then(data => {
         res.json(data)
-    }).catch(err=>{
-        res.status(500).json({msg:"this man does not exist",err})
+    }).catch(err => {
+        res.status(500).json({ msg: "this man does not exist", err })
     })
 })
 
@@ -57,14 +57,14 @@ router.delete('/:id', async (req, res) => {
     // }
     try {
         const customerData = await Customer.destroy({
-            where:{
+            where: {
                 id: req.params.id
                 // customerId: req.session.customerId,
             },
         });
 
-        if(!customerData) {
-            res.status(404).json({ message: "No client found with this id"})
+        if (!customerData) {
+            res.status(404).json({ message: "No client found with this id" })
             return;
         }
 
@@ -77,9 +77,9 @@ router.delete('/:id', async (req, res) => {
 // edit profile
 router.put('/:id', (req, res) => {
     Customer.update({
-        customer_name: req.body.customer_name,
-        customer_password: req.body.customer_password,
-        customer_phone_number: req.body.customer_phone_number
+        name: req.body.customer_name,
+        password: req.body.customer_password,
+        phone_number: req.body.customer_phone_number
     }).then(data => {
         res.json(data)
     }).catch(err => {
@@ -128,10 +128,10 @@ router.post("/login", (req, res) => [
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
-            res.status(200).json({msg: 'logged out!'});
+            res.status(200).json({ msg: 'logged out!' });
         });
     } else {
-        res.status(401).json({msg: "you must be logged in!"});
+        res.status(401).json({ msg: "you must be logged in!" });
     }
 });
 
